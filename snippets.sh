9e5b8207eacf02730ab704db687d1550bc8655ec
Szymon Sobanski <sobanski.s@gmail.com>
2018-05-08 09:45:41 +0200

    Add new git alias for push. Add snippet for updating chef.

--- snippets.sh
+++ snippets.sh
@@ -1,4 +1,7 @@
 # switch off output buffering in grep
 stdbuf -o 0 grep my_pattern
 # use above to have live updates
-tail -f something.log | stdbuf -o 0 grep my_patter
+tail -f something.log | stdbuf -o 0 grep my_pattern
+
+# update chef client to a specific version
+(echo "version=$VERSION"; curl -L https://www.opscode.com/chef/install.sh) | sudo bash
